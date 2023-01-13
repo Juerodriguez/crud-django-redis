ba22aa6b759d5a83b0940836bf6ec3ea4d553a2a
Juan Elias Rodriguez <stylorj@gmail.com>
2023-01-13 14:42:32 -0300

    create schema

---
+++ app/main/schemas/books_schema.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from uuid import uuid4
+
+
+def generate_uuid():
+    return str(uuid4())
+
+
+def generate_date():
+    return str(datetime.now())
+
+
+class Books(BaseModel):
+    id: str = Field(default_factory=generate_uuid())
+    name: str
+    author: str
+    date: str = Field(default_factory=generate_date())
